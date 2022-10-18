a0bbcf92bb7787700503611f7c11ee0aeeb3bcd8
mcsel <marcel.selle@sms-group.com>
2022-10-18 13:27:53 +0200

    done

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const apiURL = "https://api.adviceslip.com/advice";
 const adviceText = document.getElementById('advice');
 const adviceId = document.getElementById('adviceId');
 
-fetch(apiURL).then((response) => {
-  return response.json();
-}).then((data) => {
-  adviceText.innerText = data['slip']['advice']
-  adviceId.innerText = data['slip']['id']
-})
+refresh();
+
+function refresh(){
+  fetch(apiURL).then((response) => {
+    return response.json();
+  }).then((data) => {
+    adviceText.innerText ="\""+ data['slip']['advice'] +"\""
+    adviceId.innerText = data['slip']['id']
+  })
+}
